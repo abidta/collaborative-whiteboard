c71bc6d4bb6a9201727290964078ec51efbc2f99
root <root@zainulabid>
2024-04-09 12:49:49 +0530

    patch: add env port, add type for env

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,19 @@ import indexRouter from "./routes/indexRouter";
 const app: Application = express();
 const server = createServer(app);
 const io = new Server(server);
+const PORT = process.env.PORT || 3000;
+const HOST: string = "";
 socket(io);
 
 app.use(logger("dev"));
 app.use(express.static("./public"));
 app.use("/", indexRouter);
-//vie engine setup
+
+//view engine setup
 app.engine(".hbs", engine({ extname: ".hbs", defaultLayout: false }));
 app.set("views", "./views");
 app.set("view engine", "hbs");
 
-server.listen(3000, () => {
-  console.log("server running");
-});
+server.listen(PORT, HOST, () =>
+  console.log(`Server listenning in ${PORT}`)
+);
--- src/socket.ts
+++ src/socket.ts
@@ -1,30 +1,40 @@
 import { addUser, deleteUser, getUser, getUserCount } from "./users";
+
 export default (io: any) => {
   io.on("connection", (socket: any): void => {
     console.log("user connected", socket.id);
-    addUser(socket.id)
-    io.emit("new-user",getUserCount());
+
+    addUser(socket.id);
+
+    io.emit("new-user", getUserCount());
+
     socket.on("new-added", (object: any): void => {
       console.log("object added");
       socket.broadcast.emit("new-added", object);
     });
+
     socket.on("mod-obj", (object: any): void => {
       socket.broadcast.emit("mod-obj", object);
     });
+
     socket.on("mousemove", (mouseCoord: any): void => {
       mouseCoord.id = socket.id;
       socket.broadcast.emit("mousemove", mouseCoord);
     });
+
     socket.on("undo-redo", (data: string): void => {
       socket.broadcast.emit("undo-redo", data);
     });
+
     socket.on("clear", (): void => {
       socket.broadcast.emit("clear");
     });
 
     socket.on("disconnect", (): void => {
-      deleteUser(socket.id)
-      io.emit("disconnect-user",getUserCount(),socket.id)
+      deleteUser(socket.id);
+
+      io.emit("disconnect-user", getUserCount(), socket.id);
+
       console.log("user disconnected");
     });
   });
